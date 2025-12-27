344b0597e6a30b608d514ba4091b8ab98d9d6e36
KindredS0ULzGH <kindreds0ulzbackup@gmail.com>
2025-12-26 19:53:23 -0500

    Update index.js

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ async function run() {
   form.append(
     "payload_json",
     JSON.stringify({
-      content: " 📅 **New Week, New Stream Schedule!**",
+      content: "<@&1353762877705682984> 📅 **New Week, New Stream Schedule!**",
       embeds: [
         {
           title: "This Week's Stream Schedule",
